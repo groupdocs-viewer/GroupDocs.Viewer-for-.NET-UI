569320ed2b213e87e16dd53b9b734a94a36e5333
Vladimir Litvinchik <vladimir.litvinchik@aspose.com>
2021-09-05 23:19:17 +0300

    Fixed names of builder classes.

---
+++ src/GroupDocs.Viewer.UI.Api.Local.Cache/GroupDocsViewerUIApiBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using GroupDocs.Viewer.UI.Core;
+using GroupDocs.Viewer.UI.Api.Local.Cache;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class GroupDocsViewerUIApiBuilderExtensions
+    {
+        public static GroupDocsViewerUIApiBuilder AddLocalCache(this GroupDocsViewerUIApiBuilder builder, string cachePath)
+        {
+            ServiceDescriptor registeredServiceDescriptor = builder.Services.FirstOrDefault(
+                s => s.ServiceType == typeof(IFileCache));
+
+            if (registeredServiceDescriptor != null)
+            {
+                builder.Services.Remove(registeredServiceDescriptor);
+            }
+
+            // NOTE: Replace is used here as by default we've registered Noop cache 
+            builder.Services.AddTransient<IFileCache>(_ => new FileLocalCache(cachePath));
+
+            return builder;
+        }
+    }
+}
---
+++ src/GroupDocs.Viewer.UI.SelfHost.Api/Extensions/MvcBuilderExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using GroupDocs.Viewer.UI.Api.Controllers;
+using GroupDocs.Viewer.UI.Core;
+using GroupDocs.Viewer.UI.SelfHost.Api.Configuration;
+using GroupDocs.Viewer.UI.SelfHost.Api.Licensing;
+using GroupDocs.Viewer.UI.SelfHost.Api.Viewers;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class MvcBuilderExtensions
+    {
+        public static GroupDocsViewerUIApiBuilder AddGroupDocsViewerSelfHostApi(this IMvcBuilder builder, 
+            Action<Config> setupConfig = null)
+        {
+            var config = new Config();
+            setupConfig?.Invoke(config);
+
+            // GroupDocs.Viewer API Registration
+            builder.PartManager.ApplicationParts.Add(new AssemblyPart(
+                Assembly.GetAssembly(typeof(ViewerController))));
+
+            builder.Services
+                .AddOptions<Config>()
+                .Configure<IConfiguration>((settings, configuration) =>
+                {
+                    configuration.BindSelfHostApiSettings(settings);
+                    setupConfig?.Invoke(settings);
+                });
+
+            builder.Services.AddSingleton<IViewerLicenser, ViewerLicenser>();
+            builder.Services.AddTransient<IFileCache, NoopFileCache>();
+
+            switch (config.ViewerType)
+            {
+                case ViewerType.HtmlWithExternalResources:
+                    builder.Services.AddTransient<IViewer, HtmlWithExternalResourcesViewer>();
+                    break;
+                case ViewerType.Png:
+                    builder.Services.AddTransient<IViewer, PngViewer>();
+                    break;
+                case ViewerType.Jpg:
+                    builder.Services.AddTransient<IViewer, JpgViewer>();
+                    break;
+                default:
+                    builder.Services.AddTransient<IViewer, HtmlWithEmbeddedResourcesViewer>();
+                    break;
+            }
+
+            return new GroupDocsViewerUIApiBuilder(builder.Services);
+        }
+    }
+}
