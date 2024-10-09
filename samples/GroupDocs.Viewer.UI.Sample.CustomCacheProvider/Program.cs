65442d13029241e3552f696cb9a27250dc6bbfdb
Vladimir Litvinchik <vladimir.litvinchik@aspose.com>
2024-10-09 21:42:03 +0300

    Added example for custom cache using ConcurrentDictionary

---
+++ samples/GroupDocs.Viewer.UI.Sample.CustomCacheProvider/Program.cs
@@ -0,0 +1,79 @@
+using System.Collections.Concurrent;
+using GroupDocs.Viewer.UI.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddGroupDocsViewerUI();
+
+builder.Services
+    .AddControllers()
+    .AddGroupDocsViewerSelfHostApi(config =>
+    {
+        //Trial limitations https://docs.groupdocs.com/viewer/net/evaluation-limitations-and-licensing-of-groupdocs-viewer/
+        //Temporary license can be requested at https://purchase.groupdocs.com/temporary-license
+        //config.SetLicensePath("c:\\licenses\\GroupDocs.Viewer.lic"); // or set environment variable 'GROUPDOCS_LIC_PATH'
+    })
+    .AddLocalStorage("./Files");
+
+//NOTE: should be registered after AddGroupDocsViewerSelfHostApi()
+builder.Services.AddSingleton<IFileCache, ConcurrentDictionaryFileCache>();
+
+var app = builder.Build();
+
+app
+    .UseRouting()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapGet("/", async context =>
+        {
+            await context.Response.WriteAsync("Viewer UI can be accessed at '/viewer' endpoint.");
+        });
+        endpoints.MapGroupDocsViewerUI(options =>
+        {
+            options.UIPath = "/viewer";
+            options.APIEndpoint = "/viewer-api";
+        });
+        endpoints.MapGroupDocsViewerApi(options =>
+        {
+            options.ApiPath = "/viewer-api";
+        });
+    });
+
+app.Run();
+
+class ConcurrentDictionaryFileCache : IFileCache
+{
+    private readonly ConcurrentDictionary<string, object> _cache =
+        new ConcurrentDictionary<string, object>();
+
+    public TEntry TryGetValue<TEntry>(string cacheKey, string filePath)
+    {
+        string key = $"{filePath}_{cacheKey}";
+        if (_cache.TryGetValue(key, out object obj))
+            return (TEntry)obj;
+
+        return default!;
+    }
+
+    public Task<TEntry> TryGetValueAsync<TEntry>(string cacheKey, string filePath)
+    {
+        TEntry entry = TryGetValue<TEntry>(cacheKey, filePath);
+        return Task.FromResult(entry);
+    }
+
+    public void Set<TEntry>(string cacheKey, string filePath, TEntry entry)
+    {
+        if(entry is null)
+            throw new ArgumentNullException(nameof(entry));
+
+        string key = $"{filePath}_{cacheKey}";
+        _cache.TryAdd(key, entry);
+    }
+
+    public Task SetAsync<TEntry>(string cacheKey, string filePath, TEntry entry)
+    {
+        Set(cacheKey, filePath, entry);
+        return Task.CompletedTask;
+    }
+}
