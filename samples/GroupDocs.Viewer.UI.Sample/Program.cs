db61f165a81bcfc7b98cea2167b11e217200709d
Viktor Stupak <viktor.stupak@aspose.com>
2024-12-12 17:24:05 +0100

    add new version of AnularUI

--- samples/GroupDocs.Viewer.UI.Sample/Program.cs
+++ samples/GroupDocs.Viewer.UI.Sample/Program.cs
@@ -4,22 +4,10 @@ using GroupDocs.Viewer.UI.Core.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 var viewerType = ViewerType.HtmlWithEmbeddedResources;
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        policyBuilder =>
-        {
-            policyBuilder
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-        });
-});
+
 builder.Services
     .AddGroupDocsViewerUI(config =>
     {
-        config.InitialFile = "annual-review.docx";
-
         config.PreloadPages = 3; // Number of pages to create on first request
         config.DefaultLanguage = LanguageCode.English;
         config.SupportedLanguages = [LanguageCode.French, LanguageCode.English, LanguageCode.Italian];
@@ -33,13 +21,12 @@ builder.Services
 
         //Trial limitations https://docs.groupdocs.com/viewer/net/evaluation-limitations-and-licensing-of-groupdocs-viewer/
         //Temporary license can be requested at https://purchase.groupdocs.com/temporary-license
-        config.SetLicensePath("c:\\licenses\\GroupDocs.Viewer.lic"); // or set environment variable 'GROUPDOCS_LIC_PATH'
+        //config.SetLicensePath("c:\\licenses\\GroupDocs.Viewer.lic"); // or set environment variable 'GROUPDOCS_LIC_PATH'
     })
     .AddLocalStorage("./Files")
     .AddLocalCache("./Cache");
 
 var app = builder.Build();
-app.UseCors();
 app
     .UseRouting()
     .UseEndpoints(endpoints =>
